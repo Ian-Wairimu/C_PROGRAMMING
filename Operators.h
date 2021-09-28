610910d5843aeca70268580469aa6730a1ca9d5d
wairimu ian <wairimuian@gmail.com>
2021-09-28 09:33:54 +0300

    added my c_programming commit

---
+++ Operators.h
@@ -0,0 +1,9 @@
+//
+// Created by Student5 on 9/28/2021.
+//
+
+#ifndef TUTORIAL_OPERATORS_H
+#define TUTORIAL_OPERATORS_H
+
+#endif //TUTORIAL_OPERATORS_H
+#include <stdio.h>
---
+++ main.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main(){
+    //declared the int
+    int x = 0, y = 0, result = 0;
+    //requesting the user to input first number
+    printf("Enter first No: \n");
+    scanf("%i", &x);
+    //request the user to input second number
+    printf("Enter second No: \n");
+    scanf("%i", &y);
+    //this works the summation of the two number
+    result = x + y;
+    //this prints the summation of the two number in the console
+    printf("Results is: %i", result);
+    //outputs depend on what the user will enter
+
+    return 0; //which stands for return true if it is one it will be return false
+}
